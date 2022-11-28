e4fb0364ec841c66a007e9c5f19be86ae56bcf28
Davide05 <113897151+Davide05@users.noreply.github.com>
2022-11-28 01:54:46 +0100

    Update File_A.cpp

--- FILE/File_A.cpp
+++ FILE/File_A.cpp
@@ -6,47 +6,46 @@
 * @version 1.0 <10.11.2022> Versione iniziale
 * @version 1.1 <data> <Descrivere le modifiche apportate>
 */
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//= = = INCLUDES = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#include <stdio.h>		//per printf/scanf
-#include <stdlib.h>		//per system
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//= = = MAIN = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//= = = INCLUDES = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#include <stdio.h>						//per printf/scanf
+#include <stdlib.h>						//per system
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//= = = MAIN = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 int main()
 {
 	char Es_A[]="Es_A.txt";					//nome del primo File
 	char Es_A1[]="Es_A1.txt";				//nome del secondo File
 	int err, err1;								
-	char c;									//carattere
-	FILE *pFile;							//puntaFile		
+	char c;							//carattere
+	FILE *pFile;						//puntaFile		
 	FILE *pFile1;
 	printf("Il contenuto del primo FILE:\n\n");
 	
-    pFile=fopen(Es_A, "r");					//apre il File
+    	pFile=fopen(Es_A, "r");					//apre il File
     
-	while(!feof(pFile))					//
+	while(!feof(pFile))					//controlla se finisce il FILE
 	{
 		c= fgetc(pFile);				//prende la lettera
-		printf("%c", c);					//stampa
+		printf("%c", c);				//stampa
 	}
 	err= fclose(pFile);					
 	printf("\n");
 		
 	printf("Adesso il programma riapre in un nuovo FILE lo stesso contenuto tutto in maiscolo\n");
 	system("pause");
-//
-//
-	pFile=fopen(Es_A, "r");			    //apre il File
+	
+	pFile=fopen(Es_A, "r");			 		//apre il File
 	pFile1=fopen(Es_A1, "w");
 	printf("\n");
-	while(!feof(pFile))					//
+	while(!feof(pFile))					//controlla se finisce il FILE
 	{
 		c= fgetc(pFile);				//prende la lettera
 		if(c>='a' && c<='z')				//converte tutto in maiscolo
 		c-=32;				
-		printf("%c", c);					//stampa
+		printf("%c", c);				//stampa
 		if(!feof(pFile))				//per togliere l'ultimo carattere in piu'
 		fputc(c,pFile1);
 	}
@@ -55,5 +54,3 @@ int main()
 	printf("\n\n");
 	system("pause");
 }
-
-
