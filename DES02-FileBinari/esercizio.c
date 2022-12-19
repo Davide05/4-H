c6d8c9852d70ee757bb86505865953fe9ca9add5
Davide05 <huzsdavide@gmail.com>
2022-12-19 10:46:22 +0000

    file binari

--- DES02-FileBinari/esercizio.c
+++ DES02-FileBinari/esercizio.c
@@ -17,7 +17,7 @@
 #include <stdio.h>							//per printf/scanf
 #include <stdlib.h>							//per system
 #define V 8									//voti
-#define L 10								//lunghezza stringa del nome
+#define L 10								//lunghezza stringa del nome e del mese
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 //= = = FUNZIONI = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -32,80 +32,82 @@ int numeroRecord(char fileName[]);
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 struct data 							
 {
-	int gg;									//giorno
-	int mm; 								//mese
-	int aa;									//anno
-}typedef data; 								//nome di defoult data
+	int gg;			//giorno
+	char mm[L]; 	//mese
+	int aa;			//anno
+}typedef data; 		//nome di defoult data
 
 struct stud 								
 {
 	char cognome[L];
 	data nascita;
 	int  voti[V];
-}typedef stud; 								//nome di defoult stud
+}typedef stud; 		//nome di defoult stud
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 //= = = MAIN = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 int main()
 {
-	char file []="datifile.txt";					//nome del primo File
+	char fileName[]="datifile.txt";	//nome del File da aprire
 	int err, numRecord;							
-	char c;											//carattere
-	FILE *pf;										//puntaFile	
-
-	if(pf==NULL)
-	{
-		printf("\nIL FILE NON PUO'ESSERE APERTO!\n");
-		return 0;
-	}
-		printf("inserisci il numero di record che voresti creare:\t");
-		scanf("%d", &numRecord);
-		pf=fopen(file, "r+");						//apre il File
-		inserisciRecord(file, numRecord);
-		
-    
-		while(!feof(pf))							//controlla se finisce il FILE
-		{
-			c= fgetc(pf);							//prende la lettera
-			printf("%c", c);						//stampa
-		}	
-		
-		err= fclose(pf);					
-		printf("\n");		
-		system("pause");
+	char c;
 	
-		//if(!feof(pf))						//per togliere l'ultimo carattere in piu'
-		
-		err= fclose(pf);					
-		printf("\n\n");
-		system("pause");
-		return 0;
+	printf("inserisci il numero di record che voresti creare:\t");
+	scanf("%d", &numRecord);
+	inserisciRecord(fileName, numRecord);
+
+	stampaFile(fileName);
+/*
+	while(!feof(pf))				//controlla se finisce il FILE
+	{
+	c= fgetc(pf);					//prende la lettera
+	printf("%c", c);				//stampa
+	}	
+
+	err= fclose(pf);					
+	printf("\n");		
+	system("pause");
+
+	//if(!feof(pf))						//per togliere l'ultimo carattere in piu'
+
+	err= fclose(pf);					
+	printf("\n\n");
+	system("pause");
+*/
+	return 0;
 }												
 
 /** ****************************************************************************************
-* @brief <inserire in seguito n record chiesti dall'utente dal main>
-* @param  <elenco dei parametri in ingresso alla funzione>
-* @retval <valori restituiti>
-* @see <See Also: Describes a cross-reference to classes, functions, methods, variables, ...>
+* @brief Inserire in seguito n record chiesti
+* @param file: nome del file da aprire; numRecord: numero di record da inserire
+* @retval nessuno
+* @see stampaRecord
 *
 * @author Davide Hu
-* @version 1.0 <19.12.2022> <Versione iniziale>
-* @version 1.1 <data> <Descrivere le modifiche apportate>
+* @version 1.0 19.12.2022 Versione iniziale
 */
 void inserisciRecord(char file[], int numRecord)
 {	
-	stud recordino; 							//dichiarazione di un record
-	int err, cont=1; 							//per far funzionare le funzioni di file
+	stud recordino;
+	int err, cont=1;
 	FILE* pf; 								
+
+	pf=fopen(file, "ab");			//apre il File
+	if(pf==NULL)
+	{
+		printf("\nIL FILE NON PUO'ESSERE APERTO!\n");
+		return;
+	}
+
 	for(int i=0; i<numRecord; i++)
 	{
 		printf("\n%d^ studente:\n", cont); cont++;
-		printf("\ninserisci il cognome dello studente:\t");            scanf("%s",  recordino.cognome);
-		printf("\ninserisci il giorno di nascita:\t");                 scanf("%d", &recordino.nascita.gg);
-		printf("\ninserisci il mese di nascita(in lettere):\t");       scanf("%s",  recordino.nascita.mm);
-		printf("\ninserisci l'anno di nascita:\t");                    scanf("%d", &recordino.nascita.aa);
+		printf("inserisci il cognome:\t");            			scanf("%s",  recordino.cognome);
+		printf("inserisci il giorno di nascita:\t");            scanf("%d", &recordino.nascita.gg);
+		printf("inserisci il mese di nascita(in lettere):\t");  scanf("%s",  recordino.nascita.mm);
+		printf("inserisci l'anno di nascita:\t");               scanf("%d", &recordino.nascita.aa);
 		printf("** *************************************************************************************");
-		for(int j=0; j<V; j++)
+		for(int j=0; j<V; j++) //TODO: inseriti dall'utente
 		{
 			int a=rand()%10+1;
 			recordino.voti[j]=a;
@@ -114,7 +116,7 @@ void inserisciRecord(char file[], int numRecord)
 }
 
 /** ****************************************************************************************
-* @brief <inserire una breve descrizione della routine>
+* @brief  Stampa su monitor (una riga per record) tutte le informazioni contenute nel file; 
 * @param  <elenco dei parametri in ingresso alla funzione>
 * @retval <valori restituiti>
 * @see <See Also: Describes a cross-reference to classes, functions, methods, variables, ...>
@@ -123,7 +125,10 @@ void inserisciRecord(char file[], int numRecord)
 * @version 1.0 <15.12.2022> <Versione iniziale>
 * @version 1.1 <data> <Descrivere le modifiche apportate>
 */
-
+void stampaFile(char fileName[]) 
+{
+	;
+}
 /** ****************************************************************************************
 * @brief <inserire una breve descrizione della routine>
 * @param  <elenco dei parametri in ingresso alla funzione>
